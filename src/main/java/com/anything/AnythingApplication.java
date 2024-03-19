9adad2ede873f955f536530b6a3e7314cb306fe3
kimjoochang <kimjc0707@naver.com>
2024-03-20 00:04:30 +0900

    프로젝트 생성

---
+++ src/main/java/com/anything/AnythingApplication.java
@@ -0,0 +1,13 @@
+package com.anything;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnythingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnythingApplication.class, args);
+	}
+
+}
